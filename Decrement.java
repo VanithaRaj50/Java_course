1fb6e363749cd4cbd4d54500600a584dd28d9d2a
VanithaRaj50 <91432179+VanithaRaj50@users.noreply.github.com>
2021-09-27 17:11:16 +0530

    Add files via upload

---
+++ Decrement.java
@@ -0,0 +1,16 @@
+class Decrement{
+public static void main(String[] args)
+{
+System.out.print("Pre increment");
+int m=10;
+int n=--m;
+System.out.println("--m :" + m);
+System.out.println("n:" + n);
+
+System.out.println("Post decrement");
+int i=50;
+int j=i--;
+System.out.println("i-- :" + i);
+System.out.println("j :" + j);
+}
+}
