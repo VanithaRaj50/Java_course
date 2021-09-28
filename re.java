4a3771baf5b0ee6326aa40da57be4040dcc27e57
VanithaRaj50 <91432179+VanithaRaj50@users.noreply.github.com>
2021-09-28 09:20:32 +0530

    Add files via upload

---
+++ re.java
@@ -0,0 +1,28 @@
+class RelationalOperatorDemo{
+public static void main(String[] args)
+{
+int i=10;
+int j=20;
+int k=20;
+boolean result1=i<j;
+boolean result2=j<i;
+boolean result3=i>j;
+boolean result4=j>i;
+boolean result5=i<=k;
+boolean result6=i>=j;
+boolean result7=i==k;
+boolean result8=i!=k;
+boolean result9=j!=k;
+boolean result10=j==k;
+System.out.println("result is:"+result1);
+System.out.println("result is:"+result2);
+System.out.println("result is:"+result3);
+System.out.println("result is:"+result4);
+System.out.println("result is:"+result5);
+System.out.println("result is:"+result6);
+System.out.println("result is:"+result7);
+System.out.println("result is:"+result8);
+System.out.println("result is:"+result9);
+System.out.println("result is:"+result10);
+}
+}
