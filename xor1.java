808ff6e923bb73d3c84eeaede1a3b739d485a603
VanithaRaj50 <91432179+VanithaRaj50@users.noreply.github.com>
2021-09-29 19:48:33 +0530

    Add files via upload

---
+++ xor1.java
@@ -0,0 +1,9 @@
+class XorOperatorDemo{
+public static void main(String[] args)
+{int a=9;
+int b=15;
+System.out.println("9 XOR 15:"+(9^15));
+System.out.println("1 XOR 20:"+(1^20));
+System.out.println("7 XOR 7:"+(7^7));
+}
+}
