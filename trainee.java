00befe613d695041b722fea751cd1aac23f5d629
VanithaRaj50 <91432179+VanithaRaj50@users.noreply.github.com>
2021-09-30 19:30:33 +0530

    Add files via upload

---
+++ trainee.java
@@ -0,0 +1,9 @@
+class Trainee{
+public static void main(String []a){
+boolean isCameraOn=true;
+if(isCameraOn==true){
+System.out.println("Able to see everyone");
+}else
+System.out.println("Removed from meeting");
+}
+}
