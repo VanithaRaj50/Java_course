cc16091096c4b121a7cd5213dcc055f2ef907496
VanithaRaj50 <91432179+VanithaRaj50@users.noreply.github.com>
2021-10-01 20:12:40 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,21 @@
+class DataTypesTest{
+public static void main(String[]args){
+byte b=10;
+System.out.println("b:"+b);
+short s=3000;
+System.out.println("s:"+s);
+int i=10000;
+System.out.println("i:"+i);
+long l=10000000;
+System.out.println("1:"+1);
+float f=10.00f;
+System.out.println("f:"+f);
+double d=20.120;
+System.out.println("d:"+d);
+char c='2';
+System.out.println("c:"+c);
+boolean boo1=true;
+System.out.println("boo1:"+boo1);
+}
+}
+
