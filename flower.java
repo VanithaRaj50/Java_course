a9f8c1950bb1efca9520b31e86facf06ba5cbab0
VanithaRaj50 <91432179+VanithaRaj50@users.noreply.github.com>
2021-10-07 20:09:29 +0530

    Add files via upload

---
+++ flower.java
@@ -0,0 +1,31 @@
+class flower{
+String name;
+String color;
+int nopfpetals;
+void bloom(){
+System.out.println(`color+""+name+"is blooming");
+}
+void shedfragrnance(){
+System.out.println(color+""+name+"is shedding fragnance");
+}
+public static void main(String[] args)
+Flower rose=new Flower();
+rose.name=Rose";
+rose.color="yellow";
+rose.nopfpetals=16;
+Flower Jasmine=new Flower();
+jasmine.name="Jasmine";
+jasmine.noofpetals=7;
+jasmine.bloom();
+rose.bloom();
+jasmine.shedFragnance();
+rose.shedFragnance();
+Flower lotus=new Flower();
+lotus.name="Lotus";
+lotus.color="Pink";
+lotus.noofpetals=16;
+lotus.bloom();
+lotus.shedFragnance();
+new Flower().bloom();
+}
+
